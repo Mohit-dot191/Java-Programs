ef96635e70f6641dd62fe4df55b0f6d9162092e2
Mohit-dot191 <mohitpant150@gmail.com>
2021-05-11 19:30:27 +0700

    Create only one object in this code

---
+++ SingletonPattern/AppConfig.java
@@ -0,0 +1,20 @@
+package SingletonPattern;
+
+public class AppConfig {
+
+	private AppConfig()
+	{
+		
+	}	
+		private static AppConfig obj = new AppConfig();
+		public static AppConfig getInstance()
+		{
+			if(obj == null)
+			{
+				obj = new AppConfig();
+			}
+			return obj;
+		}
+	}
+
+
