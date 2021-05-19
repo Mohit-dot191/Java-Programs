9cfd94c8593c66adbf89253410cc031ce271d9d5
Mohit-dot191 <mohitpant150@gmail.com>
2021-05-19 15:41:37 +0700

    Genrate Random Number

---
+++ RandomNumber.java
@@ -0,0 +1,13 @@
+package Introduction;
+
+public class RandomNumber {
+
+	public static void main(String[] args) {
+		
+		System.out.println("First Random no = "+ Math.random());
+		System.out.println("Second Random no = "+ Math.random());
+		System.out.println("Third Random no = "+ Math.random());
+		System.out.println("Fourth Random no ="+Math.random());
+	}
+
+}
