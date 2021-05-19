7123ae4823661c56dffe70c65c9271b4049261d0
Mohit-dot191 <mohitpant150@gmail.com>
2021-05-19 15:42:27 +0700

    All Ascii value 0 to 256

---
+++ AsciiValue.java
@@ -0,0 +1,14 @@
+package Introduction;
+
+public class AsciiValue {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<257;i++)
+		{
+			System.out.println("The acsii value of "+(char)i+" "+i);
+		}
+
+	}
+
+}
